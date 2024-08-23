a2c5a6dd2e4f2ec1e74d0fb139399fe58ba91376
Dzmitry <whitesurfer.89@gmail.com>
2024-08-23 13:18:44 +0300

    feat:add burger menu

--- shelter/page/main/burgermenu.js
+++ shelter/page/main/burgermenu.js
@@ -1,11 +1,30 @@
 // burger handler
-(function () {
-  const burgerItem = document.querySelector(".header__menu-burger");
-  const menu = document.querySelector(".nav__list");
+'use strict'
 
-  burgerItem.addEventListener('click', () => {
-    burgerItem.classList.toggle('active');
-    menu.classList.toggle('active');
-  });
 
-}());
+  (function () {
+    const burgerItem = document.querySelector(".header__menu-burger");
+    const menu = document.querySelector(".nav__list");
+    const bodyContainer = document.querySelector('.page-body');
+    const overlayWrap = document.querySelector(".overlay");
+
+    burgerItem.addEventListener('click', () => {
+      burgerItem.classList.toggle('active');
+      menu.classList.toggle('active');
+      bodyContainer.classList.toggle('menu-opened');
+      overlayWrap.classList.toggle('overlay-active');
+    });
+
+    overlayWrap.addEventListener('click', () => {
+      burgerItem.classList.remove("active");
+      menu.classList.remove("active");
+      overlayWrap.classList.remove("overlay-active");
+    });
+
+    document.querySelectorAll(".nav__item").forEach(n => n.addEventListener("click", () => {
+      burgerItem.classList.remove("active");
+      menu.classList.remove("active");
+      overlayWrap.classList.remove("overlay-active");
+    }))
+
+  }());
